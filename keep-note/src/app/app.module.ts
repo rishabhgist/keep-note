9f0fe4eed39aa7a3c734f5322e599af4cdce3c1e
Rishabh Gist <damyrachit@gmail.com>
2022-11-06 19:09:00 +0530

    added keepnote navbar

--- keep-note/src/app/app.module.ts
+++ keep-note/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule, BrowserAnimationsModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
